9a0218583a4206f2c093261a2b13efe72565bc86
nl2412 <nghiaanduc@gmail.com>
2019-11-04 13:15:17 +0100

    fix indent display error on github

--- src/client/python_client.py
+++ src/client/python_client.py
@@ -3,27 +3,27 @@ import socket
 
 class TcpClient():
     def __init__(self):
-    	self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def establish_connection(self, host, port):
-    	self.socket.connect((host, port))
-    	print("Connection to ('{}', {}) established.".format(host, port))
+        self.socket.connect((host, port))
+        print("Connection to ('{}', {}) established.".format(host, port))
 
 
 def main():
-	tcp_client = TcpClient()
-	tcp_client.establish_connection("127.0.0.1", 9998)
-	while True:
-		message = input('Enter message here: ')
-		message_encode = message.encode()
-		tcp_client.socket.sendall(message_encode)
-		while True:
-			data = tcp_client.socket.recv(1024)
-			if data.decode() == "done":
-				break
-			else:
-				print(data.decode())
+    tcp_client = TcpClient()
+    tcp_client.establish_connection("127.0.0.1", 9998)
+    while True:
+        message = input('Enter message here: ')
+        message_encode = message.encode()
+        tcp_client.socket.sendall(message_encode)
+        while True:
+            data = tcp_client.socket.recv(1024)
+            if data.decode() == "done":
+                break
+            else:
+                print(data.decode())
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/server/server.py
+++ src/server/server.py
@@ -7,103 +7,103 @@ import math
 
 
 class SoundDetector():
-	def __init__(self, threshold):
-		self.chunk = 1024
-		self.format = pyaudio.paInt16
-		self.channels = 1
-		self.rate = 44100
-		self.record_seconds = 5
-		self.swidth = 2
-		self.threshold = threshold
-		self.short_normalize = (1.0/32768.0)
-		self.pyrecord = pyaudio.PyAudio()
+    def __init__(self, threshold):
+        self.chunk = 1024
+        self.format = pyaudio.paInt16
+        self.channels = 1
+        self.rate = 44100
+        self.record_seconds = 5
+        self.swidth = 2
+        self.threshold = threshold
+        self.short_normalize = (1.0/32768.0)
+        self.pyrecord = pyaudio.PyAudio()
 
-	def convert_data(self, input):
-		count = len(input) / self.swidth
-		format = "%dh" % (count)
-		shorts = struct.unpack(format, input)
-		sum_square = 0.0
-		for sample in shorts:
-			n = sample * self.short_normalize
-			sum_square += n*n
-		rms = math.pow(sum_square/count, 0.5)
-		return rms * 1000
+    def convert_data(self, input):
+        count = len(input) / self.swidth
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, input)
+        sum_square = 0.0
+        for sample in shorts:
+            n = sample * self.short_normalize
+            sum_square += n*n
+        rms = math.pow(sum_square/count, 0.5)
+        return rms * 1000
 
-	def start_recording(self, connection):
-		stream = self.pyrecord.open(format=self.format, channels=self.channels,
-									rate=self.rate, input=True, output=True,
-									frames_per_buffer=self.chunk)
-		print("start recording")
-		for i in range(0, int(44100 / self.chunk * self.record_seconds)):
-			data = stream.read(self.chunk)
-			rms_value = self.convert_data(data)
-			if rms_value > self.threshold:
-				message = "Sound detected"
-				# send data to client
-				connection.sendall(message.encode())
+    def start_recording(self, connection):
+        stream = self.pyrecord.open(format=self.format, channels=self.channels,
+                                    rate=self.rate, input=True, output=True,
+                                    frames_per_buffer=self.chunk)
+        print("start recording")
+        for i in range(0, int(44100 / self.chunk * self.record_seconds)):
+            data = stream.read(self.chunk)
+            rms_value = self.convert_data(data)
+            if rms_value > self.threshold:
+                message = "Sound detected"
+                # send data to client
+                connection.sendall(message.encode())
 
-		# done recording
-		print("recording done")
-		message = "done"
-		connection.sendall(message.encode())
+        # done recording
+        print("recording done")
+        message = "done"
+        connection.sendall(message.encode())
 
-		stream.stop_stream()
-		stream.close()
-		self.pyrecord.terminate()
+        stream.stop_stream()
+        stream.close()
+        self.pyrecord.terminate()
 
 
 class TcpServer():
-	def __init__(self, host, port):
-		self.server_host = host
-		self.server_port = port
-		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, host, port):
+        self.server_host = host
+        self.server_port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		# bind listener to specified port
-		self.server_socket.bind((self.server_host, self.server_port))
+        # bind listener to specified port
+        self.server_socket.bind((self.server_host, self.server_port))
 
-		print("Server setup successfully.")
+        print("Server setup successfully.")
 
-	def start(self):
-		# start listening for incoming connection
-		self.server_socket.listen(1)
-		print("Listening for connection from client...")
+    def start(self):
+        # start listening for incoming connection
+        self.server_socket.listen(1)
+        print("Listening for connection from client...")
 
-		while True:
-			# accept connection from client
-			conn, client_address = self.server_socket.accept()
-			try:
-				print("Received connection from {}".format(client_address))
-				while True:
-					# read request from client(s)
-					data = conn.recv(1024)
-					print("Received {!r}".format(data.decode()))
+        while True:
+            # accept connection from client
+            conn, client_address = self.server_socket.accept()
+            try:
+                print("Received connection from {}".format(client_address))
+                while True:
+                    # read request from client(s)
+                    data = conn.recv(1024)
+                    print("Received {!r}".format(data.decode()))
 
-					if data:
-						detector = SoundDetector(20)
-						detector.start_recording(conn)
-					else:
-						break
+                    if data:
+                        detector = SoundDetector(20)
+                        detector.start_recording(conn)
+                    else:
+                        break
 
-			finally:
-				conn.close()
+            finally:
+                conn.close()
 
 
 async def main():
-	# setup server: TcpServer(host, port)
-	tcp_server = TcpServer("", 9998)
-	# start a task that will listen for connection from clients asynchronously
-	task = loop.create_task(tcp_server.start())
-	await asyncio.wait(task)
+    # setup server: TcpServer(host, port)
+    tcp_server = TcpServer("", 9998)
+    # start a task that will listen for connection from clients asynchronously
+    task = loop.create_task(tcp_server.start())
+    await asyncio.wait(task)
 
 if __name__ == "__main__":
-	try:
-		# create an event loop for asyncio
-		loop = asyncio.get_event_loop()
+    try:
+        # create an event loop for asyncio
+        loop = asyncio.get_event_loop()
 
-		# start the loop until main function completed its job
-		loop.run_until_complete(main())
-	except Exception as err:
-		raise err
-	finally:
-		# close loop on finish
-		loop.close()
+        # start the loop until main function completed its job
+        loop.run_until_complete(main())
+    except Exception as err:
+        raise err
+    finally:
+        # close loop on finish
+        loop.close()
